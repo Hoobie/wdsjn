a48f86de8ce61227ef895ba9ffa0c8d3209bc823
Hoobie <hoobie92@gmail.com>
2016-12-09 09:55:38 +0100

    Extract a results' path to the variable

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,7 +1,9 @@
 :: For Windows only
 
+SET RESULTS_PATH=///c/Users/hoobie/Source/Repos/wdsjn/results/:/usr/src/app/results/
+
 ECHO Rebuild Docker image
 CALL docker build -t wdsjn .
 
 ECHO Run container
-CALL docker run -v ///c/Users/hoobie/Source/Repos/wdsjn/results/:/usr/src/app/results/ -it --rm wdsjn
+CALL docker run -v %RESULTS_PATH% -it --rm wdsjn
